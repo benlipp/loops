3f4a03f5ca812312957a5ef16dfebabaa5fff35f
Ben Lippincott <benji.lippincott@gmail.com>
2017-04-10 16:44:34 -0400

    added project new loop button

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -16,7 +16,15 @@
                 @foreach($projects as $project)
                     <div class="col-md-4">
                         <div class="panel panel-default">
-                            <div class="panel-heading"><h2 class="panel-title">{{ $project->name }}</h2></div>
+                            <div class="panel-heading clearfix">
+                                <div class="pull-right">
+                                    <button class="btn btn-default btn-xs"
+                                            onclick="newLoop({{ $project->id }})">
+                                        <span class="glyphicon glyphicon-plus"></span>
+                                    </button>
+                                </div>
+                                <h2 class="panel-title">{{ $project->name }}</h2>
+                            </div>
                             <div class="panel-body">
                                 @foreach($project->loops()->open()->get() as $l)
                                     <p><span class="glyphicon glyphicon-refresh"></span> {{ $l->name }}</p>
@@ -33,4 +41,14 @@
         </div>
     </div>
     @include('dashboard._new-loop')
+@endsection
+
+@section('scripts')
+    <script>
+        function newLoop(project_id)
+        {
+            $("select[name='project']").val(project_id);
+            $('#new-loop-modal').modal();
+        }
+    </script>
 @endsection
