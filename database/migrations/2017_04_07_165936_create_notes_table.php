dcd3f47143decb76bbe7c64a816f8625929ecffd
Ben Lippincott <benji.lippincott@gmail.com>
2017-04-10 18:09:30 -0400

    laravel, get sane defaults please

--- database/migrations/2017_04_07_165936_create_notes_table.php
+++ database/migrations/2017_04_07_165936_create_notes_table.php
@@ -15,7 +15,7 @@ class CreateNotesTable extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('body');
+            $table->text('body');
             $table->integer('notable_id')->unsigned();
             $table->string('notable_type');
             $table->integer('author_id')->unsigned();
