340df49fca1c0c960fef46d8d0ab54087ecf3b38
Ben Lippincott <benji.lippincott@gmail.com>
2017-04-12 09:55:17 -0400

    add uuids

--- config/app.php
+++ config/app.php
@@ -226,6 +226,11 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        /*
+         * Custom Aliases
+         */
+        'Uuid' => Webpatser\Uuid\Uuid::class,
+
     ],
 
 ];
